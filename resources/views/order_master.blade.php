18d7b1e1bfd6d7454676bface718ff9ec0c8ed17
kerolesmonsef <keroles.monsef@foo.mobi>
2024-02-02 02:57:35 +0200

    city area search

--- resources/views/order_master.blade.php
+++ resources/views/order_master.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ asset("css/bootstrap.min.css") }}" rel="stylesheet">
+{{--  icon  --}}
+    <link rel="icon" href="{{ asset("images/dark-logo.png") }}" type="image/x-icon">
     <title>Trx</title>
 </head>
 <body class="" style="background-color: #131416" dir="rtl">
