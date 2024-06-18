6087b186421bb1b405441ec86f92904894d9c6eb
kerolesmonsef <keroles.monsef@foo.mobi>
2024-06-18 15:13:03 +0300

    fixed database ports

--- public/js/main.js
+++ public/js/main.js
@@ -16,25 +16,3 @@ function syntaxHighlight(json) {
         return '<span class="' + cls + '">' + match + '</span>';
     });
 }
-
-
-$(document).on('click','.copy-uuid',function() {
-    var url = $(this).data('url');
-    copyToClipboard(url);
-
-    // Change button class to 'warning'
-    $(this).removeClass('btn-dark').addClass('btn-warning');
-
-    // Make other buttons with class 'btn-dark' have their classes changed to 'btn-dark'
-    $('.copy-uuid').not(this).removeClass('btn-dark').addClass('btn-dark');
-});
-
-
-function copyToClipboard(text) {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val(text).select();
-    document.execCommand("copy");
-    $temp.remove();
-}
-
